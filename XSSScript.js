80d7ccf033811f620b2c438eba23cf53e3e129de
praveennathan1992 <134770563+praveennathan1992@users.noreply.github.com>
2023-05-27 17:43:18 +0530

    Add files via upload

---
+++ XSSScript.js
@@ -0,0 +1,11 @@
+function callInput(){
+        var input = document.getElementById('input-test').value;
+        var testHtml = document.getElementById('test-html');
+        var testInnerHTML = document.getElementById('test-innerHTML');
+        $(testHtml).html(input);
+        testInnerHTML.innerHTML = input; // ...; my%20name=John%20Smith
+    }
+function WriteCookie () {
+       Cookies.set('flag3',1234567890 ) 
+       Cookies.set('dummy','dhdddhgdgdhgddhdghdvghdggddfgfdgdfgdfgdfgf')
+}
