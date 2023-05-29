8cf9a6fd5d3bb4891dc28d1d8d8d05b1c448766a
praveennathan1992 <134770563+praveennathan1992@users.noreply.github.com>
2023-05-29 09:56:01 +0530

    Update XSSScript.js

--- XSSScript.js
+++ XSSScript.js
@@ -7,5 +7,5 @@ function callInput(){
     }
 function WriteCookie () {
        Cookies.set('token', 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQ3Jvc3NTaXRlIFNjcmlwdGluZyIsIkZMQUdfMzoiOiJuSlVNNEtFWWRENG1TTWFuIiwiSXNzdWVyIjoiTmV4dHVwbGUiLCJVc2VybmFtZSI6IlhTUyIsImV4cCI6MTY4NTMyNzY3NywiaWF0IjoxNjg1MzI3Njc3fQ.zh352IRPtaVCD0-RCGedHvO7bSW5xwC7MIxEm6a0SnY' ) 
-       Cookies.set('lorem','Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua')
+       Cookies.set('lorem', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua')
 }
